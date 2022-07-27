197bc66db422a584683bad7b10e25622e3d9ce16
Shalane Proctor <shalane088@gmail.com>
2022-07-26 19:55:34 -0500

    Added create functionality, form, and button click for adding player.

--- pages/team.js
+++ pages/team.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { getPlayers } from '../api/playerData';
@@ -25,9 +26,11 @@ export default function TeamPage() {
     >
       <h3>Please enjoy creating fantasy space race players for the Galactic Troopers!</h3>
       <p>The Galactic Troopers are a strong team led by a fearless captain! They not only participate in the Space Race, but during their off seasons they bravely explore through the depths of the universe.</p>
-      <Button variant="info" type="button" size="lg" className="copy-btn">
-        Add Player
-      </Button>
+      <Link href="/team/new" passHref>
+        <Button variant="info" type="button" size="lg" className="copy-btn">
+          Add Player
+        </Button>
+      </Link>
       <div className="flex-wrap">
         {players.map((player) => (
           <PlayerCard key={player.firebaseKey} playerObj={player} onUpdate={getAllPlayers} />
